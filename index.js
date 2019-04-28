f0db01a8e80d10f93895afc5270aa70845138a65
Dil Sombayeva <dilyasom@gmail.com>
2019-04-27 19:05:59 -0700

    get endpoints

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/api/zoos', async (req, res) => {
+ try {
+  const type = await db('zoos');
+  res.status(200).json(type);
+ } catch (error) {
+  res.status(500).json(error);
+ }
+});
+
+server.get('/api/zoos/:id', async (req, res) => {
+ try {
+  const type = await db('zoos').where('id', req.params.id);
+  if (type.length > 0) {
+   res.status(200).json(type);
+  } else {
+   res.status(404).json({ message: 'No record with this id' });
+  }
+ } catch (error) {
+  res.status(500).json(error);
+ }
+});
+
 
 const port = 3300;
 server.listen(port, function() {
